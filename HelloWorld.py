f32c7bd6d779f438dabf8a6bfd43b2097371ad2d
amiriello <amiriello@gmail.com>
2021-03-06 12:46:57 -0500

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -4,9 +4,23 @@ class MyFrame(wx.Frame):
     def __init__(self):
         super().__init__(parent=None, title='Hello World')
         panel = wx.Panel(self)
-
-        self.text_ctrl = wx.TextCtrl(panel, pos=(5, 5))
-        my_btn = wx.Button(panel, label='Press Me', pos=(5, 55))
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        button = wx.BoxSizer(wx.HORIZONTAL)
+        flex = wx.FlexGridSizer(2, 3, 3, 3)
+        
+        caltext = wx.StaticText(panel, label="CAL")
+        
+        inputcal = wx.TextCtrl(panel)
+        buttoncal = wx.Button(panel, label='...')
+        #lashbutton = wx.Button(panel, label='Flash')
+        
+        flex.AddMany([ (caltext), (inputcal, 1, wx.EXPAND), (buttoncal, 1, wx.EXPAND)])
+        flex.AddGrowableRow (1, 1)
+        flex.AddGrowableCol (1, 1)
+        
+        hbox.Add(flex, proportion=1, flag=wx.ALL | wx.EXPAND, border=10)
+        #button.Add(flashbutton)
+        panel.SetSizer(hbox)
 
         self.Show()
 
